8f34ce1e9997b9e2e16610ae5b5a0f3a5c772888
Rhishavhere <rhishh.me@gmail.com>
2025-01-01 16:58:29 +0530

    readme update

--- src/visualization.py
+++ src/visualization.py
@@ -2,18 +2,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 class EnergyVisualizer:
-    """
-    Class for creating visualizations of energy consumption data
-    """
     @staticmethod
     def plot_prediction_accuracy(y_test, y_pred):
-        """
-        Create scatter plot of prediction accuracy
-        
-        Args:
-            y_test (array-like): Actual test values
-            y_pred (array-like): Predicted values
-        """
+
         plt.figure(figsize=(10, 5))
         plt.scatter(y_test, y_pred, alpha=0.5)
         plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2)
@@ -24,12 +15,7 @@ class EnergyVisualizer:
     
     @staticmethod
     def plot_feature_importance(feature_importance):
-        """
-        Create bar plot of feature importances
-        
-        Args:
-            feature_importance (pandas.DataFrame): Feature importance data
-        """
+
         plt.figure(figsize=(10, 5))
         feature_importance.plot(x='feature', y='importance', kind='bar')
         plt.title('Feature Importance for Power Prediction')
@@ -41,12 +27,7 @@ class EnergyVisualizer:
     
     @staticmethod
     def plot_hourly_consumption(hourly_consumption):
-        """
-        Create line plot of hourly energy consumption
-        
-        Args:
-            hourly_consumption (pandas.Series): Average consumption by hour
-        """
+
         plt.figure(figsize=(12, 5))
         hourly_consumption.plot(kind='line', marker='o')
         plt.title('Average Energy Consumption by Hour')
