bbe5d50997da4a81169eb6604f2ebbe02490a5d1
Rhishavhere <rhishh.me@gmail.com>
2025-01-01 23:44:23 +0530

    update

--- compare.py
+++ compare.py
@@ -15,21 +15,21 @@ def read_predicted_consumption(file_path):
 def generate_real_time_data(predicted_consumption):
     real_time_data = []
     for predicted_value in predicted_consumption:
-        variation = random.uniform(-0.2, 1)  
+        variation = random.uniform(-0.2, 0.5)  
         real_time_value = predicted_value + variation
         real_time_data.append(max(0, real_time_value))
     return real_time_data
 
 def compare_consumption(predicted_consumption, real_time_consumption):
     start_time = datetime.now()
-    for hour, (predicted, real) in enumerate(zip(predicted_consumption, real_time_consumption)):
-        current_time = start_time + timedelta(seconds=hour)
+    for minute, (predicted, real) in enumerate(zip(predicted_consumption, real_time_consumption)):
+        current_time = start_time + timedelta(seconds=minute*60)
 
         if real > predicted * 1.5:
-            print(f"Alert! {current_time.strftime('%Y-%m-%d %H:%M:%S')} - Consumption is much higher: Predicted {predicted:.2f}, Real {real:.2f}")
+            print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')} - Consumption is much higher: Predicted {predicted:.2f}, Real {real:.2f}   ALERT")
         else:
             print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')} - Consumption is normal: Predicted {predicted:.2f}, Real {real:.2f}")
-        time.sleep(1)  
+        time.sleep(0.2)  
 
 if __name__ == "__main__":
     file_path = '../Backend/outputs/hourly_consumption.csv'
